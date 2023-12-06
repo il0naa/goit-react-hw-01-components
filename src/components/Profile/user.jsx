6cb36493fbdfe73db56cdadcd303eedd4c32c7d9
il0naa <136170246+il0naa@users.noreply.github.com>
2023-12-06 23:05:06 +0100

    profile

---
+++ src/components/Profile/user.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Profile from './path/to/Profile';
+import user from './path/to/user.json';
+
+const App = () => {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
+
+export default App;
