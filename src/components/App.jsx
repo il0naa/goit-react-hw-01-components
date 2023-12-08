ab9e0250fde9a267b66b5350d461bdb759d5afa5
il0naa <136170246+il0naa@users.noreply.github.com>
2023-12-08 10:42:01 +0100

    add profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import Profile from "./Profile/Profile";
+import user from "../data/user.json";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="container">
+      <Profile
+          avatar={user.avatar}
+          tag={user.tag}
+          username={user.username}
+          location={user.location}
+          stats={user.stats}
+      />
     </div>
   );
 };
