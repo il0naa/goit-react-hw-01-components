5452e2dbe1765e173dc1ee43100953a17c9a4876
il0naa <136170246+il0naa@users.noreply.github.com>
2023-12-10 13:21:26 +0100

    fixed profile

--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -13,23 +13,23 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
             className={`${css.avatar}`}
           />
           </div>
-          <p className="name">{username}</p>
-          <p className="tag">{tag}</p>
-          <p className="location">{location}</p>
+          <p className={`${css.name}`}>{username}</p>
+          <p className={`${css.tag}`}>{tag}</p>
+          <p className={`${css.location}`}>{location}</p>
         </div>
       
-        <ul className="stats">
-          <li>
-            <span className="label">Followers</span>
-            <span className="quantity">{followers}</span>
+        <ul className={`${css.stats}`}>
+          <li className={`${css.item}`}>
+            <span className={`${css.label}`}>Followers</span>
+            <span className={`${css.quantity}`}>{followers}</span>
           </li>
-          <li>
-            <span className="label">Views</span>
-            <span className="quantity">{views}</span>
+          <li className={`${css.item}`}>
+            <span className={`${css.label}`}>Views</span>
+            <span className={`${css.quantity}`}>{views}</span>
           </li>
-          <li>
-            <span className="label">Likes</span>
-            <span className="quantity">{likes}</span>
+          <li className={`${css.item}`}>
+            <span className={`${css.label}`}>Likes</span>
+            <span className={`${css.quantity}`}>{likes}</span>
           </li>
         </ul>
       </div>
