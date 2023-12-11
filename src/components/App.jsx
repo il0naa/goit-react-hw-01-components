6b3424a4430e262f337450c9b713c8ce9a54779d
il0naa <136170246+il0naa@users.noreply.github.com>
2023-12-11 12:25:06 +0100

    add friend list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import Profile from "./Profile/Profile";
 import Statistics from "./Statistics/Statistics";
+import FriendList from "./FriendList/FriendList";
 import user from "../data/user.json";
 import data  from "../data/data.json";
+import friends from "../data/friends.json"
 
 export const App = () => {
   return (
@@ -16,7 +18,9 @@ export const App = () => {
       <Statistics
         stats={data}
         title="Upload stats"
-         />
+       />
+      <FriendList
+        friends={friends} />
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,19 @@
+import css from 'components/FriendList/FriendList.module.css'
+
+const FriendListItem = ({ avatar, name, isOnline }) => (
+    <li className={`${css.item}`}>
+        <span className={isOnline ? `${css.onlineStyle}` : `${css.offlineStyle}`}></span>
+        <img className={`${css.avatar}`} src={avatar} alt={name} width="48" />
+        <p className={`${css.name}`}>{name}</p>
+    </li>
+  );
+  
+  const FriendList = ({ friends }) => (
+    <ul className={`${css.friendList}`}>
+      {friends.map(({ id, ...friend }) => (
+        <FriendListItem key={id} {...friend} />
+      ))}
+    </ul>
+  );
+  
+  export default FriendList;
