e2b7a4553422f248cc94e43ec8ce29e5f91e824c
il0naa <136170246+il0naa@users.noreply.github.com>
2023-12-11 13:52:46 +0100

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,8 @@ import FriendList from "./FriendList/FriendList";
 import TransactionHistory from "./TransactionHistory/TransactionHistory";
 import user from "../data/user.json";
 import data  from "../data/data.json";
-import friends from "../data/friends.json"
-import transactions from "../data/transactions.json"
+import friends from "../data/friends.json";
+import transactions from "../data/transactions.json";
 
 export const App = () => {
   return (
